3b1591e66dd766b2c4e44ece69cb76bfecde7040
Mosikanan <mosikanan@senzmate.com7>
2026-03-17 00:00:58 +0530

    ui

--- mobile/jest.config.js
+++ mobile/jest.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   preset: 'react-native',
+  moduleNameMapper: {
+    '\\.(ttf)$': '<rootDir>/__mocks__/fileMock.js',
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native|@react-native|@react-native-community|react-native-vector-icons)/)',
+  ],
 };
