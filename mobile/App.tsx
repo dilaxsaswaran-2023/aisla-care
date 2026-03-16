75d6e46cd889432b76d2740dfc1a554bf3394ea6
Mosikanan <mosikanan@senzmate.com7>
2026-03-17 00:38:49 +0530

    val

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -2,7 +2,10 @@ import React from 'react';
 import {StatusBar, StyleSheet, View} from 'react-native';
 
 import {colors} from './src/constants/colors';
+import {applyPoppinsDefaults} from './src/constants/typography';
 import {RootNavigator} from './src/navigation/RootNavigator';
+
+applyPoppinsDefaults();
 
 export default function App(): React.JSX.Element {
   return (
